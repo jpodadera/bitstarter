5a715a934c228ecb63134847aab0438eeea7f8e0
ubuntu <ubuntu@ubuntu.(none)>
2013-07-07 20:19:02 +0200

    Updated checksjson and grader.js as HW3 Part3

--- grader.js
+++ grader.js
@@ -24,24 +24,33 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
+var restler = require('restler');
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    if (!fs.existsSync(instr)){
+	console.log("%s does not exist. Exiting.", instr);
+	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
 
+
 var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+    return cheerio.load(htmlfile);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
+};
+
+var checkHtmlUrl = function(urlarg, checkfile) {
+    restler.get(urlarg.toString()).on('success', function(data) {
+	var checkJson = checkHtmlFile(data, checkfile);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+	});
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
@@ -64,11 +73,11 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --urlfile <url_file>', 'URL to html file')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    //console.log("Urlfile: %s", program.urlfile);
+    //console.log("Checksfile: %s", program.checks);
+    checkHtmlUrl(program.urlfile, program.checks);
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+    exports.checkHtmlUrl = checkHtmlUrl;
 }
